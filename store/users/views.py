6233b87fb10c1b8177bcdaa3d0a49bd934a70943
sergei <sergrb27@yandex.ru>
2023-02-08 17:00:44 +0300

    users form auth

--- store/users/views.py
+++ store/users/views.py
@@ -4,7 +4,17 @@ from users.forms import UserLoginForm
 
 
 def login(request):
-    context = {'form': UserLoginForm()}
+    if request.method == 'POST':
+        form = UserLoginForm(data=request.POST)
+        if form.is_valid():
+            username = request.POST['username']
+            password = request.POST['password']
+            user = auth.authenticate(username=username, password=password)
+            if user:
+                auth.login(request, user)
+    else:
+        form = UserLoginForm()
+    context = {'form': form}
     return render(request, 'users/login.html', context)
 
 
