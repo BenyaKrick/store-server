efda4f60d011d1afc67b64e3ea44d58b2041c3f9
sergei <sergrb27@yandex.ru>
2023-02-15 17:01:16 +0300

    create basket

--- store/users/views.py
+++ store/users/views.py
@@ -3,6 +3,7 @@ from django.contrib import auth, messages
 from users.models import User
 from users.forms import UserLoginForm, UserRegistrationForm, UserProfileForm
 from django.urls import reverse
+from products.models import Basket
 
 
 def login(request):
@@ -44,7 +45,11 @@ def profile(request):
             print(form.errors)
     else:
         form = UserProfileForm(instance=request.user)
-    context = {'title': 'Store - Профиль', 'form': form}
+    context = {
+        'title': 'Store - Профиль',
+        'form': form,
+        'baskets': Basket.objects.all(),
+    }
     return render(request, 'users/profile.html', context)
 
 
