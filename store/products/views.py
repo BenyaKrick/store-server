3a1795c3063fc3b803eedc3d59e3bed863fe2226
benyakrck <sergrb27@yandex.ru>
2023-02-25 19:02:26 +0300

    пагинатор страниц

--- store/products/views.py
+++ store/products/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, HttpResponseRedirect
 from products.models import ProductCategory, Product, Basket
 from users.models import User
 from django.contrib.auth.decorators import login_required
+from django.core.paginator import Paginator
 
 
 def index(request):
@@ -11,18 +12,23 @@ def index(request):
     return render(request, 'products/index.html', context)
 
 
-def products(request, category_id=None):
+def products(request, category_id=None, page_number=1):
+    products = Product.objects.filter(category_id=category_id) if category_id else Product.objects.all()
+
+    per_page = 2
+    paginator = Paginator(products, per_page)
+    products_paginator = paginator.page(page_number)
+
     context = {
         'title': 'Каталог',
         'categories': ProductCategory.objects.all(),
-        'products': Product.objects.filter(category_id=category_id) if category_id else Product.objects.all(),
+        'products': products_paginator,
     }
     return render(request, 'products/products.html', context)
 
 
 @login_required
 def basket_add(request, product_id):
-
     product = Product.objects.get(id=product_id)
     baskets = Basket.objects.filter(user=request.user, product=product)
 
