7a9c2aaaf04835f185c9043f579c45102a9270b5
benyakrck <sergrb27@yandex.ru>
2023-02-25 20:19:33 +0300

    admin

--- store/products/admin.py
+++ store/products/admin.py
@@ -1,4 +1,18 @@
 from django.contrib import admin
-from .models import ProductCategory, Product
-admin.site.register(Product)
+from .models import ProductCategory, Product, Basket
+
 admin.site.register(ProductCategory)
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('name', 'price', 'quantity', 'category')
+    fields = ('name', 'description', ('price', 'quantity'), 'image', 'category')
+    search_fields = ('name', 'price')
+    ordering = ('name',)
+
+
+class BasketAdmin(admin.TabularInline):
+    model = Basket
+    fields = ('product', 'quantity')
+    extra = 0
