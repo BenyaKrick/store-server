26d04b6a95898044870eb19a20523ca6f7e66a08
sergei <sergrb27@yandex.ru>
2023-03-06 17:06:48 +0300

    create send email

--- store/users/admin.py
+++ store/users/admin.py
@@ -1,7 +1,8 @@
+
 from django.contrib import admin
 
-from users.models import User, EmailVerification
 from products.admin import BasketAdmin
+from users.models import EmailVerification, User
 
 
 @admin.register(User)
@@ -13,5 +14,5 @@ class UserAdmin(admin.ModelAdmin):
 @admin.register(EmailVerification)
 class EmailVerificationAdmin(admin.ModelAdmin):
     list_display = ('code', 'user', 'expiration')
-    fieldsets = ('code', 'user', 'expiration', 'created')
+    fields = ('code', 'user', 'expiration', 'created')
     readonly_fields = ('created',)
